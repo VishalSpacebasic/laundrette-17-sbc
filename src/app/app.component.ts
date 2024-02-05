102b256636b510552ce16c25069aa0eccd1fdeea
VishalSpacebasic <112153924+VishalSpacebasic@users.noreply.github.com>
2024-02-05 11:46:31 +0530

    added qr scanner

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-
+import { NgxScannerQrcodeModule, LOAD_WASM } from 'ngx-scanner-qrcode';
+import { AsyncPipe, JsonPipe } from '@angular/common';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NgxScannerQrcodeModule,AsyncPipe,JsonPipe],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'laundrette-17-sbc';
